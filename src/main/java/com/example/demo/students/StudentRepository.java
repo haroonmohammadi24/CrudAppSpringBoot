24f34d1ff67d9178b5dabb220fca6174adcb4ab9
haroonmohammadi24 <mohharoonmohammadi24@gmail.com>
2023-05-22 06:47:41 +0430

    i addded the void get method

--- src/main/java/com/example/demo/students/StudentRepository.java
+++ src/main/java/com/example/demo/students/StudentRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface StudentRepository extends JpaRepository<Student, Long> {
 	 Optional<Student> findStudentByEmail(String email);
 
+	 public void get();
+
 }
